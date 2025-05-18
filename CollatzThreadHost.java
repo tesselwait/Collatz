5643300d24666a0a356cd9a13c14bf368ddfc1f6
Robert Larson <tesselwait@users.noreply.github.com>
2025-05-18 01:25:16 -0400

    Update CollatzThreadHost.java

--- CollatzThreadHost.java
+++ CollatzThreadHost.java
@@ -52,7 +52,7 @@ public class CollatzThreadHost {
 		System.out.println("Results:");
 		for(ArrayList<Object> b: getData()){
 			System.out.printf("Base: %,.0f ; Match: "+b.get(0)+", Permutations: "+b.get(1), b.get(2));
-			if(b.get(3)!=null)
+			if(b.size()>3)
 				System.out.print(", sequence: "+b.get(3);
 			System.out.println();
 		}
@@ -66,10 +66,10 @@ public class CollatzThreadHost {
 		double totalTestStrings=10000000000.0; 
 		int cores = Runtime.getRuntime().availableProcessors();
 		CollatzSequenceGenerator seqGen = new CollatzSequenceGenerator(100000000000.0, 13);
-		SequenceTree tree = seqGen.constructTree(seqGen.generateBestMatchPermutation(100, 20)); //tree generator bugged
+		SequenceTree tree = seqGen.constructTree(seqGen.generateBestMatchPermutation(100, 20));
 		CollatzThreadHost crawler = new CollatzThreadHost(totalTestStrings, totalTestStrings/(1.0*cores));
 		for(int i=0; i<cores; i++) {
-			CollatzClosestPermutationCombo4 object = new CollatzClosestPermutationCombo4(0.0+i*crawler.threadSet, crawler.threadSet, tree, "Thread "+i, crawler);
+			CollatzClosestPermutationCombo4 object = new CollatzClosestPermutationCombo4(0.0+i*crawler.threadSet, crawler.threadSet, tree, "Thread "+i, crawler);  // runs about half as fast as single sequence crawler
 			//CollatzClosestPermutationCombo object = new CollatzClosestPermutationCombo(0.0+i*crawler.threadSet, crawler.threadSet, "10101010101010101010101000000000000000010101010101010101010101000010101010100000000101010101010101010101010000000010101010101010101010101", "Thread "+i, crawler);
 			//CollatzClosestPermutationCombo object = new CollatzClosestPermutationCombo(0.0+i*crawler.threadSet, crawler.threadSet, seqGen.generateBestMatchPermutation(50, 5), "Thread "+i, crawler);
 			// ^ Integrates sequence generator directly into crawler.  Tests list of sequences rather than one.  Comparative slowdown: (sequences per segment)^(number of segments).  Segmented version or constructing a tree should be faster
